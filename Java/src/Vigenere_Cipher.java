877c27f46bb452917e24de191214942211014f60
Nicolas-Mousten <nicolas.mousten@gmail.com>
2021-09-28 14:31:55 +0200

    vigenere works

--- Java/src/Vigenere_Cipher.java
+++ Java/src/Vigenere_Cipher.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 public class Vigenere_Cipher {
+    public static String ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static String abc = "abcdefghijklmnopqrstuvwxyz";
+    public static String abcCom = ABC+abc;
 
     public static Scanner scanner(){
         Scanner scanner = new Scanner(System.in);
@@ -9,24 +11,49 @@ public class Vigenere_Cipher {
     }
     //-----------------------------------Tools above--------------------------------------------------
     public static void main(String[] args) {
+
+        System.out.println(abcCom);
+        //System.out.println(abcCom.length());
         scanner();
         System.out.print("Encrypt:1 / Decrypt:2 ");
         String choice = scanner().nextLine();
         if(choice.equalsIgnoreCase("1")){
             encrypt();
         }else if(choice.equalsIgnoreCase("2")){
-
+            decrypt();
         }
     }
     public static void encrypt(){
         scanner();
+        System.out.println("Choose your key: ");
+        String key = "Kea";
+        //String key = scanner().nextLine();
+        System.out.println("Input message to encrypt: ");
+        String message = "Helloxyz";
+        //String message = scanner().nextLine();
+        int[] convertedMessageToInt = stringToIntArray(message);
+        int[] convertedKeyToInt = stringToIntArray(key);
+
+    }
+    public static void decrypt(){
+        scanner();
         System.out.print("Choose your key: ");
         String key = scanner().nextLine();
-        System.out.print("Input message to encrypt: ");
+        System.out.print("Input message to decrypt: ");
         String message = scanner().nextLine();
+
     }
     //------------------------------------Functions below---------------------------------------------
-    //Make an array that saves the entire word whitout the blank spaces, then use index of the word to remake them to numbers.
-    //Problem is that i don't know how many words there will be and can't create arrays for the numbers after.
-    //Mabey i can make a master array to keep the format right.
+    public static int[] stringToIntArray(String input){
+        int stringLen = input.length();
+        int[] StringToIntArray = new int[stringLen];
+        for (int i = 0; i < stringLen; i++) {
+            StringToIntArray[i] = abcCom.indexOf(input.charAt(i)+1);
+            System.out.println(StringToIntArray[i]);
+        }
+        return StringToIntArray;
+    }
+    //public static int[] combineIntArray(int[] arrayKey, int[] arrayMessage){
+
+    //}
 }
--- Java/src/Vigenère_Cipher_Dosent_Work_With_Blank_Spaces.java
+++ Java/src/Vigenère_Cipher_Dosent_Work_With_Blank_Spaces.java
@@ -2,7 +2,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Vigenère_Cipher_Dosent_Work_With_Blank_Spaces {
-    public static String abc = "abcdefghijklmnopqrstuvwxyz";
+    public static String abc = "abcdefghijklmnopqrstuvwxyz ,.!";
 
     public static Scanner scanner(){
         Scanner scanner = new Scanner(System.in);
@@ -16,7 +16,7 @@ public class Vigenère_Cipher_Dosent_Work_With_Blank_Spaces {
         if(choice.equalsIgnoreCase("1")){
             encrypt();
         }else if(choice.equalsIgnoreCase("2")){
-
+            decrypt();
         }
     }
     public static void encrypt(){
@@ -31,6 +31,21 @@ public class Vigenère_Cipher_Dosent_Work_With_Blank_Spaces {
         for (int i = 0; i < combinedMessageAndKey.length; i++) {
             System.out.print(abc.charAt(combinedMessageAndKey[i]));
         }
+        System.out.println("");
+        decrypt();
+    }
+    public static void decrypt(){
+        scanner();
+        System.out.print("Choose your key: ");
+        String key = scanner().nextLine();
+        System.out.print("Input message to decrypt: ");
+        String message = scanner().nextLine();
+        int[] convertedMessage = stringToInt(message);
+        int[] convertedKey = stringToInt(key);
+        int[] seperatedMessageAndKey = SubtractingTwoIntArraysTogether(convertedMessage,convertedKey);
+        for (int i = 0; i < seperatedMessageAndKey.length; i++) {
+            System.out.print(abc.charAt(seperatedMessageAndKey[i]));
+        }
     }
     //------------------------------------Functions below---------------------------------------------
     public static int[] stringToInt(String stringToInt){
@@ -38,7 +53,7 @@ public class Vigenère_Cipher_Dosent_Work_With_Blank_Spaces {
         int stringLen = stringToInt.length();
         int[] convertedString = new int[stringLen];
         for (int i = 0; i < stringLen; i++) {
-            int temp = abc.indexOf(stringToInt.charAt(i))+1;
+            int temp = abc.indexOf(stringToInt.charAt(i));
             convertedString[i] = temp;
         }
         return convertedString;
@@ -49,12 +64,42 @@ public class Vigenère_Cipher_Dosent_Work_With_Blank_Spaces {
         int[] combinedInt = new int[intArray1Len];
         int j = 0;
         for (int i = 0; i < intArray1Len; i++) {
-            System.out.println(abc.charAt(integerArray1[i]-1) +" "+abc.charAt(intKey[j]-1));//Check the characters in the arrays
-            combinedInt[i] = (integerArray1[i] + intKey[j])%26;
+            //System.out.println(abc.charAt((integerArray1[i])) +" "+abc.charAt((intKey[j])));//Check the characters in the arrays
+            //System.out.println(integerArray1[i]);
+            combinedInt[i] = (integerArray1[i] + intKey[j])%(abc.length());
             j = j +1;
             if(j ==intArray2Len){ //makes it so the key is repeated to match the message
                 j=0;
             }}
+        return combinedInt;
+    }
+    public static int[] SubtractingTwoIntArraysTogether(int[] integerArray1,int[]intKey) {
+        int intArray1Len = integerArray1.length;
+        int alphabetLen = abc.length();
+        int intArray2Len = intKey.length;
+        int[] combinedInt = new int[intArray1Len];
+        int j = 0;
+        for (int i = 0; i < intArray1Len; i++) {
+            //System.out.println(abc.charAt((integerArray1[i])) +" "+abc.charAt((intKey[j])));//Check the characters in the arrays
+            // (a % b + b) % b
+            //You aren't doing modulus at all. There is no modulo operator in Java. % is a remainder operator. And it dosen't work with negative numbers.
+            int input = integerArray1[i] - intKey[j];
+
+            //------------------------------------
+            int r = input % alphabetLen;
+            if (r < 0)
+            {
+                input += alphabetLen;
+            }
+            //------------------------------------------------------------------------------------------------------
+
+
+            combinedInt[i] = input;
+            j = j + 1;
+            if (j == intArray2Len) { //makes it so the key is repeated to match the message
+                j = 0;
+            }
+        }
         return combinedInt;
     }
 }
